db54ff0cc404837fd13d32fb2f7fdef9253a51b1
Davi Lima <davidamartesgit@Gmail.com>
2024-04-17 21:05:49 -0300

    Adiconando UsuarioLogin

---
+++ LojaSonhoDeCafe.Models/UsuarioLogado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonhoDeCafe.Models
+{
+    public class UsuarioLogado
+    {
+        public const string UsuarioId = "1";
+        public const int CarrinhoId = 1;
+    }
+
+}
