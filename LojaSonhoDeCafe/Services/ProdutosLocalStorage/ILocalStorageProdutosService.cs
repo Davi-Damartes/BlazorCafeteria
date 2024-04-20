4d88ce5a70000f2d460dc1070d33e6a2cf3955ad
Davi Lima <davidamartesgit@Gmail.com>
2024-04-20 14:04:21 -0300

    Adicionando Interface com métodos para fazer o LocalStorage

---
+++ LojaSonhoDeCafe/Services/ProdutosLocalStorage/ILocalStorageProdutosService.cs
@@ -0,0 +1,11 @@
+﻿using LojaSonhoDeCafe.Models.Dtos;
+
+namespace LojaSonhoDeCafe.Services.ProdutosLocalStorage
+{
+    public interface ILocalStorageProdutosService
+    {
+        Task<IEnumerable<ProdutoDto>> GetCollection();
+
+        Task RemoveCollection();
+    }
+}
