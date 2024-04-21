bacc00f177606270b9f16e833e0498e69160888c
Davi Lima <davidamartesgit@Gmail.com>
2024-04-21 17:32:30 -0300

    Add DataAnnotations no Dto de Produto

--- LojaSonhoDeCafe.Models/Dtos/ProdutoDto.cs
+++ LojaSonhoDeCafe.Models/Dtos/ProdutoDto.cs
@@ -1,19 +1,30 @@
-﻿namespace LojaSonhoDeCafe.Models.Dtos
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LojaSonhoDeCafe.Models.Dtos
 {
     public class ProdutoDto
     {
         public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(60, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string? Nome { get; set; }
 
-
+        [Required]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)
         public string? Descricao { get; set; }
 
         public string? FotoUrl { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório!!!")]
         public decimal Preco { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório!!!")]
         public int QuantidadeEmEstoque { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório!!!")]
         public int CategoriaId { get; set; }
         public string? CategoriaNome { get; set; }
 
