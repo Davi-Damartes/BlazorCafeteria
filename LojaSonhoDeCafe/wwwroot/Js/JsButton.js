e1ba7c2538c3f5f36e7cab368c426a65df806082
Davi Lima <davidamartesgit@Gmail.com>
2024-04-24 19:52:20 -0300

    adicionando função Js para mudar cor do botão.

---
+++ LojaSonhoDeCafe/wwwroot/Js/JsButton.js
@@ -0,0 +1,34 @@
+﻿const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+
+    btn.style.backgroundColor = '#ffac09';
+    btn.style.boxShadow = '0 0 40px #ffac09';
+    btn.style.transition = '.5s ease';
+});
+
+//function TornaBotaoAtualizarQuantidadeVisivel(id, visible) {
+
+//    const atualizaQuantidadeButton = document.querySelector("button[data-itemId='" + id + "']");
+
+//    if (visible == true) {
+//        atualizaQuantidadeButton.style.display = "inline-block";
+//    }
+//    else {
+//        atualizaQuantidadeButton.style.display = "none";
+//    }
+//}
+
+
+window.MudarCorDoBotaoJS = (produtoId, produtoFavorito) => {
+    var botao = document.getElementById("botao_" + produtoId);
+
+    if (produtoFavorito == true) {
+        botao.style.backgroundColor = "red";
+    }
+    else (produtoFavorito == false)
+
+    {
+        botao.style.backgroundColor = "gray";
+    }
+};
