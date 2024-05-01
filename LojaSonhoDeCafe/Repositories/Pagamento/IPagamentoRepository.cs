2c5226d2793a2fe2174c567b4bb7ca4fdc699135
Davi Lima <davidamartesgit@Gmail.com>
2024-04-30 21:36:40 -0300

    Criando Repository para gerencia o Pagamento.

---
+++ LojaSonhoDeCafe/Repositories/Pagamento/IPagamentoRepository.cs
@@ -0,0 +1,11 @@
+﻿using LojaSonhoDeCafe.Components.CarrinhoPages;
+using LojaSonhoDeCafe.Entities;
+
+namespace LojaSonhoDeCafe.Repositories.Pagamento
+{
+    public interface IPagamentoRepository
+    {
+
+        Task AdicionarPagamento(PagamentoDiario pagamentoDiario);
+    }
+}
---
+++ LojaSonhoDeCafe/Repositories/Pagamento/PagamentoRepository.cs
@@ -0,0 +1,24 @@
+﻿using LojaSonhoDeCafe.Data;
+using LojaSonhoDeCafe.Entities;
+
+namespace LojaSonhoDeCafe.Repositories.Pagamento
+{
+    public class PagamentoRepository : IPagamentoRepository
+    {
+        private readonly BancoDeDados _bancoDeDados;
+
+        public PagamentoRepository(BancoDeDados bancoDeDados)
+        {
+            _bancoDeDados = bancoDeDados;
+        }
+
+        public async Task AdicionarPagamento(PagamentoDiario pagamentoDiario)
+        {
+            if(pagamentoDiario != null)
+            {
+                await _bancoDeDados.PagamentosDiarios.AddAsync(pagamentoDiario);
+            }
+
+        }
+    }
+}
