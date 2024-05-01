182092025780e69bdb694c861e385cc1a11846e7
Davi Lima <davidamartesgit@Gmail.com>
2024-04-30 21:37:41 -0300

    Criando Services de Pagamento para validar e chamar o Repository

---
+++ LojaSonhoDeCafe/Services/PagamentoServices/IPagamentoService.cs
@@ -0,0 +1,9 @@
+﻿using LojaSonhoDeCafe.Models.Dtos;
+
+namespace LojaSonhoDeCafe.Services.PagamentoServices
+{
+    public interface IPagamentoService
+    {
+        Task AdicionarPagamento(PagamentoDiarioDto pagamentoDiarioDto);
+    }
+}
---
+++ LojaSonhoDeCafe/Services/PagamentoServices/PagamentoService.cs
@@ -0,0 +1,32 @@
+﻿using LojaSonhoDeCafe.Models.Dtos;
+using LojaSonhoDeCafe.Repositories.Pagamento;
+using SonhoDeCafe.Server.MapeandoDto;
+
+namespace LojaSonhoDeCafe.Services.PagamentoServices
+{
+    public class PagamentoService : IPagamentoService
+    {
+        private readonly IPagamentoRepository _pagamentoRepository;
+        private readonly ILogger<PagamentoService> _logger;
+
+        public PagamentoService(IPagamentoRepository pagamentoRepository, 
+                                ILogger<PagamentoService> logger)
+        {
+            _pagamentoRepository = pagamentoRepository;
+            _logger = logger;
+        }
+
+        public async Task AdicionarPagamento(PagamentoDiarioDto pagamentoDiarioDto)
+        {
+            if(pagamentoDiarioDto == null)
+            {
+                _logger.LogError("Pagamento inválido");
+                return; 
+            }
+
+            var pagamento = pagamentoDiarioDto.ConvertePagamentoDtoParaPagamento();
+
+            await _pagamentoRepository.AdicionarPagamento(pagamento);
+        }
+    }
+}
