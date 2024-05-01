b9d3730e8c7b3aed5d18fbf6e6a066a10d8bd2ef
Davi Lima <davidamartesgit@Gmail.com>
2024-04-30 21:39:54 -0300

    Dto de Pagamento para obter os dados do cliente.

---
+++ LojaSonhoDeCafe.Models/Dtos/PagamentoDiarioDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaSonhoDeCafe.Models.Dtos
+{
+    public class PagamentoDiarioDto
+    {
+        [MaxLength(100)]
+        [Required]
+        public string? Usuario { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public int TotalQuantDiaria { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalPrecoDiaria { get; set; }
+
+        [Required]
+        public EPagamentoDto EPagamento { get; set; }
+
+        [Required]
+        public DateTime HoraDoPagamento { get; } = DateTime.Now;
+    }
+
+
+}
