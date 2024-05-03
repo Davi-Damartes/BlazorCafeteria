df97879825f248e54a9fe3394b3ec90c00ef794f
Davi Lima <davidamartesgit@Gmail.com>
2024-05-03 20:55:56 -0300

    Dto de para mostrar os Dados na interface

---
+++ LojaSonhoDeCafe.Models/Dtos/RelatorioVendaMensalDto.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LojaSonhoDeCafe.Models.Dtos
+{
+    public class RelatorioVendaMensalDto
+    {
+        [Required]
+        public int QuantidadeProdutosVendidos { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PrecoTotalVendas { get; set; }
+
+        [Required]
+        public int QuantidadeUsuariosUnicos { get; set; }
+
+        [MaybeNull]
+        public int Mes { get; set; }
+        [MaybeNull]
+        public int Ano { get; set; }
+    }
+}
