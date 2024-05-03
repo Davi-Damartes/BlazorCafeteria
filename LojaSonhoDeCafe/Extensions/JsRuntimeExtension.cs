759033403c4bda8b04ba91ed87e6ac9a78870f17
Davi Lima <davidamartesgit@Gmail.com>
2024-05-03 20:59:13 -0300

    Método estático responsável pela invocação do javascript que mostrará uma notificação na tela.

---
+++ LojaSonhoDeCafe/Extensions/JsRuntimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+namespace LojaSonhoDeCafe.Extensions
+{
+    public static class JsRuntimeExtension
+    {
+        //Pagamento realizado com sucesso.
+        public static ValueTask ToastrSuccess(this IJSRuntime jSRuntime, string message)
+        {
+            return jSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        //Falha ao realizar o Pagamento.
+        public static ValueTask ToastrFail(this IJSRuntime jSRuntime, string message)
+        {
+            return jSRuntime.InvokeVoidAsync("ShowToastr", "fail", message);
+        }
+    }
+}
