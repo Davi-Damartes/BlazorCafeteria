4d1e50c34ec91cfef547e59d74b2961b65cb1004
Davi Lima <davidamartesgit@Gmail.com>
2024-05-03 20:58:03 -0300

    Adicionando Biblioteca Jquery e juntamento com um arquivo resposável pela geração de notificações para o Usuário.

---
+++ LojaSonhoDeCafe/wwwroot/Js/Common.js
@@ -0,0 +1,11 @@
+﻿window.ShowToastr = (type, message) => {
+
+    toastr.options.toastClass = "toastr";
+
+    if (type === "success") {
+        toastr.success(message, "Operação realizado com Sucesso!!!", { timeOut: 2000 });
+    }
+    if (type === "fail") {
+        toastr.error(message, "Falha ao realizado Operação!!!", { timeOut: 2000 });
+    }
+};
--- LojaSonhoDeCafe/wwwroot/Js/JsButton.js
+++
@@ -1,9 +0,0 @@
-﻿//const myModal = document.getElementById('modalButton')
-//const myInput = document.getElementById('clicarModal')
-
-//myModal.addEventListener('shown.bs.modal', () => {
-//    myInput.focus()
-//})
-
-//$('#exampleModal').modal('show');
-
