517fd36eb659924524843591b069a1e80cfae45e
Davi Lima <davidamartesgit@Gmail.com>
2024-05-04 17:01:14 -0300

    Adicionando Js para controlar o funcionamento do Botão de Favorito em Produtos.

---
+++ LojaSonhoDeCafe/wwwroot/Js/BotaoClickFavorito.js
@@ -0,0 +1,13 @@
+﻿window.mudarCorButao = function (produtoId, isFavorito) {
+    var btn = document.getElementById('btn-' + produtoId);
+    var icone = btn.querySelector('.icone-coracao');
+
+    if (isFavorito) {
+        icone.style.color = 'red';
+    }
+    else {
+        icone.style.color = 'black';
+    }
+};
+
+
