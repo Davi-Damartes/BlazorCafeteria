35b2bda047657d43f62af518f0bdd20974331b51
Davi Lima <davidamartesgit@Gmail.com>
2024-05-04 17:00:08 -0300

    Adicionando o campo [Required] para que quando se for criar um produto, sejá obrigátorio a inclusão de uma foto

--- LojaSonhoDeCafe.Models/Dtos/ProdutoDto.cs
+++ LojaSonhoDeCafe.Models/Dtos/ProdutoDto.cs
@@ -14,8 +14,11 @@ namespace LojaSonhoDeCafe.Models.Dtos
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string? Descricao { get; set; }
 
-        
+
+
+        [Required(ErrorMessage = "O campo foto é obrigatório!!!")]
         public string? FotoUrl { get; set; }
+
 
         [Required(ErrorMessage = "O campo {0} é obrigatório!!!")]
 
