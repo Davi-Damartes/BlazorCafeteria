3434cb057d2f38a65a56acadc086fdb7cca2a7ea
Davi Lima <davidamartesgit@Gmail.com>
2024-05-05 19:22:51 -0300

    Adicionando validações ao Dto e atribuindo a hora através do DateTime.Now

--- LojaSonhoDeCafe.Models/Dtos/PagamentoDiarioDto.cs
+++ LojaSonhoDeCafe.Models/Dtos/PagamentoDiarioDto.cs
@@ -1,16 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace LojaSonhoDeCafe.Models.Dtos
 {
     public class PagamentoDiarioDto
     {
         [MaxLength(100)]
-        [Required]
+        [Required(ErrorMessage = "O campo {0} não pode ser nullo!")]
         public string? Usuario { get; set; }
 
         [MaxLength(100)]
-        [Required]
+        [Required(ErrorMessage = "O campo {0} não pode ser nullo!")]
         [EmailAddress]
         public string? Email { get; set; }
 
@@ -21,11 +22,12 @@ namespace LojaSonhoDeCafe.Models.Dtos
         [Column(TypeName = "decimal(18,2)")]
         public decimal TotalPrecoDiaria { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Por favor, selecione um tipo de pagamento.")]
+        [NotNull]
         public EPagamentoDto EPagamento { get; set; }
 
         [Required]
-        public DateTime HoraDoPagamento { get; set; }
+        public DateTime HoraDoPagamento { get; set; } = DateTime.Now;
     }
 
 
