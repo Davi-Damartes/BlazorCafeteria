c33360d0cd9ba92f390c8b64fbacbfdad89bc364
Davi Lima <davidamartesgit@Gmail.com>
2024-05-06 21:29:50 -0300

    Criando Entidades para mapear os resultados da Api externa.

---
+++ LojaSonhoDeCafe/Entities/BrasilCep.cs
@@ -0,0 +1,19 @@
+﻿namespace LojaSonhoDeCafe.Entities
+{
+    public class BrasilCep
+    {
+
+        public string? Cep { get; set; }
+
+        public string? State { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Neighborhood { get; set; }
+
+        public string? Street { get; set; }
+
+        public string? Service { get; set; }
+
+    }
+}
