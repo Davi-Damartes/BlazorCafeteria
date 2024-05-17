2fc191e75d6f313c1a117bcf5b99c51caa5e4367
Davi Lima <davidamartesgit@Gmail.com>
2024-05-17 16:29:18 -0300

    Mudando o nome do Enum de "codigoDeBarra" para "boleto"

--- LojaSonhoDeCafe.Models/Dtos/EPagamentoDto.cs
+++ LojaSonhoDeCafe.Models/Dtos/EPagamentoDto.cs
@@ -1,11 +1,12 @@
-﻿namespace LojaSonhoDeCafe.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LojaSonhoDeCafe.Models.Dtos
 {
     public enum EPagamentoDto
     {
-        NaoInformado = 0,
         visa = 1,
         mastercard = 2,
         paypal = 3,
-        codigoDeBarra = 4,
+        boleto = 4,
     }
 }
