be3e1651ad7c298d91adf20c37da599219ac89f1
Davi Lima <davidamartesgit@Gmail.com>
2024-05-21 20:45:26 -0300

    Removendo pastas

--- LojaSonhoDeCafe.Client/Program.cs
+++ LojaSonhoDeCafe.Client/Program.cs
@@ -1,5 +1,4 @@
 using LojaSonhoDeCafe.Client;
-using LojaSonhoDeCafe.Client.Service;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,14 +7,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
-
-
-var baseUrl = "https://localhost:7135";
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri(baseUrl)
-});
-
-builder.Services.AddScoped<IProduto2Service, Produto2Service>();
 
 await builder.Build().RunAsync();
