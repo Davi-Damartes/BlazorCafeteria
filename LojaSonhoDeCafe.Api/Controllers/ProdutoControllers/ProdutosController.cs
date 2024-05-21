ac995a4a15db03e8b85cd27cc74b33948fb955b8
Davi Lima <davidamartesgit@Gmail.com>
2024-05-21 20:50:51 -0300

    Adicionando Crud no controller que serão as chamadas da Api

--- LojaSonhoDeCafe.Api/Controllers/ProdutoControllers/ProdutosController.cs
+++ LojaSonhoDeCafe.Api/Controllers/ProdutoControllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using LojaSonhoDeCafe.Api.Repositories.Produtos;
 using LojaSonhoDeCafe.Models.Dtos;
+using LojaSonhoDeCafe.Repositories.Produtos;
 using Microsoft.AspNetCore.Mvc;
 using SonhoDeCafe.Server.MapeandoDto;
 
@@ -17,7 +18,7 @@ namespace LojaSonhoDeCafe.Api.Controllers.CarrinhoControllers
 
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProdutoDto>>> ObterTodosItens( )
+        public async Task<ActionResult<IEnumerable<ProdutoDto>>> ObterTodosItens()
         {
             try
             {
@@ -48,13 +49,117 @@ namespace LojaSonhoDeCafe.Api.Controllers.CarrinhoControllers
                 var produto = await _produtoRepository.ObterProdutoPorId(Id);
                 if (produto is null)
                 {
-                    return NotFound("Produto não localizado");
+                    return NotFound("Produto não encontrado!");
                 }
                 else
                 {
                     var produtoDto = produto.ConvertProdutoParaDto();
                     return Ok(produtoDto);
                 }
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Erro ao acessar a base de dados");
+            }
+        }
+
+        [HttpGet]
+        [Route("{categoriaId}/ObterProdutosPorCategoria")]
+        public async Task<ActionResult<IEnumerable<ProdutoDto>>> ObterProdutosPorCategoria(int categoriaId)
+        {
+            try
+            {
+                var produtos = await _produtoRepository.ObterTodosProdutosPorCategoria(categoriaId);
+                var produtosDto = produtos.ConvertProdutosParaDto();
+                return Ok(produtosDto);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                "Erro ao acessar o banco de dados");
+            }
+        }
+
+        [HttpGet]
+        [Route("BuscarCategorias")]
+        public async Task<ActionResult<IEnumerable<CategoriaDto>>> BuscarCategorias()
+        {
+            try
+            {
+                var categorias = await _produtoRepository.ObterCategorias();
+                var categoriasDto = categorias.ConverterCategoriasParaDto();
+                return Ok(categoriasDto);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                           "Erro ao acessar o banco de dados");
+            }
+        }
+
+        [HttpGet]
+        [Route("BuscarProdutoFavoritos")]
+        public async Task<ActionResult<IEnumerable<ProdutoDto>>> BuscarProdutoFavoritos()
+        {
+            try
+            {
+                var produtosFavoritos = await _produtoRepository.ObterProdutosFavoritos();
+                if (produtosFavoritos is null)
+                {
+                    return NotFound("Não há produtos favoritos");
+                }
+                else
+                {
+                    var produtoFavoritosDtos = produtosFavoritos.ConvertProdutosParaDto();
+                    return Ok(produtoFavoritosDtos);
+                }
+            }
+
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Erro ao acessar a base de dados");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoDto>> AdicionarProduto(ProdutoDto produtoDto)
+        {
+            try
+            {
+                var produtoExiste = await _produtoRepository.ObterProdutoPorId(produtoDto.Id);
+                if (produtoExiste == null)
+                {
+                    await _produtoRepository.AdicionarNovoProdutoDto(produtoDto);
+                    return CreatedAtAction(nameof(AdicionarProduto), new { id = produtoDto.Id }, produtoDto);
+                }
+                return Conflict("Produto já existe!");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao acessar a base de dados");
+            }
+        }
+
+        [HttpDelete("{Id:Guid}")]
+        public async Task<IActionResult> DeletarProduto(Guid Id)
+        {
+            try
+            {
+                var produtoExiste = await _produtoRepository.ObterProdutoPorId(Id);
+
+                if(produtoExiste == null)
+                {
+                    return NotFound("Produto não encontrado");
+                }
+
+                await _produtoRepository.ExcluirProduto(produtoExiste.Id);
+                return Ok("Produto Excluído com sucesso!");
+
+                
             }
 
             catch (Exception)
