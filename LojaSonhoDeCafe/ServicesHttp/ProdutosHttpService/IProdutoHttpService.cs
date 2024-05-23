a431854ba18913bfb43032276e7f13141a1276c0
Davi Lima <davidamartesgit@Gmail.com>
2024-05-22 21:29:11 -0300

    Adicionando Service para fazer a requisição na Api

---
+++ LojaSonhoDeCafe/ServicesHttp/ProdutosHttpService/IProdutoHttpService.cs
@@ -0,0 +1,26 @@
+﻿using LojaSonhoDeCafe.Models.Dtos;
+
+namespace LojaSonhoDeCafe.ServicesHttp.ProdutosHttpService
+{
+    public interface IProdutoHttpService
+    {
+        Task<IEnumerable<ProdutoDto>> ObterProdutos();
+
+        Task<IEnumerable<ProdutoDto>> ObterProdutosFavoritos();
+
+        Task<ProdutoDto> ObterUmProduto(Guid Id);
+
+        Task<IEnumerable<CategoriaDto>> BuscaCategorias();
+
+        Task<IEnumerable<ProdutoDto>> BuscarItensPorCategoria(int categoriaId);
+
+        Task<ProdutoDto> AdicionarNovoProduto(ProdutoDto produto);
+
+        Task AtualizaProdutoFavorito(ProdutoDto produtoDto);
+
+        Task RemoverProduto(Guid Id);
+
+
+
+    }
+}
