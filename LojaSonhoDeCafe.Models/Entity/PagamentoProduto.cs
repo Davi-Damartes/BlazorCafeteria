cf3a84824803fea127031a64aa1b15e5716c908b
Davi Lima <davidamartesgit@Gmail.com>
2024-05-23 20:41:24 -0300

    Nova Entidade para gurdar valores do Pagamento do Produto, como Id do Produto, Quantidade, preco e nome.

---
+++ LojaSonhoDeCafe.Models/Entity/PagamentoProduto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaSonhoDeCafe.Models.Entity
+{
+    public class PagamentoProduto : BaseEntity
+    {
+        [Required]
+        public Guid ProdutoId { get; set; }
+
+        [MaxLength(200)]
+        public string? ProdutoNome { get; set; }
+
+        [Required]
+        public int QuantidadeComprada { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PrecoTotal { get; set; }
+
+        public Guid PagamentoDiarioId { get; set; }
+        public PagamentoDiario? PagamentoDiario { get; set; }
+
+
+    }
+}
