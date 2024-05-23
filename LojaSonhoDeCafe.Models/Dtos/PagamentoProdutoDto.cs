c9e8c6e7500b50f1a1e68625217b537b45703a1f
Davi Lima <davidamartesgit@Gmail.com>
2024-05-23 20:42:06 -0300

    Adicionando as propriedades no Dto de Pagamento Produto

---
+++ LojaSonhoDeCafe.Models/Dtos/PagamentoProdutoDto.cs
@@ -0,0 +1,26 @@
+﻿using LojaSonhoDeCafe.Models.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaSonhoDeCafe.Models.Dtos
+{
+    public class PagamentoProdutoDto
+    {
+        [Required]
+        public Guid ProdutoId { get; set; }
+
+        [Required]
+        public int QuantidadeComprada { get; set; }
+
+
+        [MaxLength(200)]
+        public string? ProdutoNome { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PrecoTotal { get; set; }
+
+
+    }
+}
