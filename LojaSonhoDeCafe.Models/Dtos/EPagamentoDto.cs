2e71eb6844f6093a593a7b43d1b23ee77f298d49
Davi Lima <davidamartesgit@Gmail.com>
2024-05-24 19:40:15 -0300

    Adicionando "Dinheiro" como Tipo de Pagamento

--- LojaSonhoDeCafe.Models/Dtos/EPagamentoDto.cs
+++ LojaSonhoDeCafe.Models/Dtos/EPagamentoDto.cs
@@ -1,12 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace LojaSonhoDeCafe.Models.Dtos
+﻿namespace LojaSonhoDeCafe.Models.Dtos
 {
     public enum EPagamentoDto
     {
         visa = 1,
         mastercard = 2,
         paypal = 3,
-        boleto = 4,
+        boleto = 4, 
+        Dinheiro = 5,
+
     }
 }
