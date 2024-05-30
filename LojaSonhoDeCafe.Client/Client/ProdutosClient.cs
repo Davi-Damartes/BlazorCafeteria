14d1e65050178ec2d5e1fec57a4dd0ab4c416779
Davi Lima <davidamartesgit@Gmail.com>
2024-05-30 19:09:29 -0300

    método para chamar a api do blazorServer

---
+++ LojaSonhoDeCafe.Client/Client/ProdutosClient.cs
@@ -0,0 +1,17 @@
+﻿using LojaSonhoDeCafe.Models.Dtos;
+using System.Net.Http.Json;
+
+//namespace LojaSonhoDeCafe.Client.Client
+//{
+//    public class ProdutosClient
+//    {
+//        private readonly HttpClient httpClient;
+
+//        public ProdutosClient(HttpClient httpClient)
+//        {
+//            this.httpClient = httpClient;
+//        }
+//        public async Task<IEnumerable<ProdutoDto>> ObterProdutos() =>
+//            await httpClient.GetFromJsonAsync("orders", OrderContext.Default.ListOrderWithStatus) ?? new();
+//    }
+//}
