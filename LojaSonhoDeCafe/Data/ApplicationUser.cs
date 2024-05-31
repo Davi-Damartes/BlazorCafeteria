d3497908e85dcb94f4dac45e87cc6d9f25ba6599
Davi Lima <davidamartesgit@Gmail.com>
2024-05-31 18:37:27 -0300

    Inserindo propriedade no UserIdentity

--- LojaSonhoDeCafe/Data/ApplicationUser.cs
+++ LojaSonhoDeCafe/Data/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace LojaSonhoDeCafe.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string? Nome { get; set; }
+        public DateOnly? DataNascimento { get; set; }
+
     }
 
 }
--- LojaSonhoDeCafe/Program.cs
+++ LojaSonhoDeCafe/Program.cs
@@ -55,6 +55,7 @@ builder.Services.AddDbContext<BancoDeDados>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
 builder.Services.AddServerSideBlazor().AddCircuitOptions(option => { option.DetailedErrors = true; });
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
@@ -115,8 +116,6 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-
 
 
 app.UseHttpsRedirection();
