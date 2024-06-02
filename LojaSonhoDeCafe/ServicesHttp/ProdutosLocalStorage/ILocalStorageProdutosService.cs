68077d516543074c72c97f8c26ab476540e8c5a8
Davi Lima <davidamartesgit@Gmail.com>
2024-06-02 18:25:37 -0300

    Adicionando Local Strorage para produtos

---
+++ LojaSonhoDeCafe/ServicesHttp/ProdutosLocalStorage/ILocalStorageProdutosService.cs
@@ -0,0 +1,13 @@
+﻿using LojaSonhoDeCafe.Models.Dtos;
+
+namespace LojaSonhoDeCafe.ServicesHttp.ProdutosLocalStorage
+{
+    public interface ILocalStorageProdutosService
+    {
+        Task<IEnumerable<ProdutoDto>> GetCollection();
+
+        Task AtualizarDadosLocalStorage(IEnumerable<ProdutoDto> novosDados);
+
+        Task RemoveCollection();
+    }
+}
---
+++ LojaSonhoDeCafe/ServicesHttp/ProdutosLocalStorage/LocalStorageProdutosService.cs
@@ -0,0 +1,56 @@
+﻿using Blazored.LocalStorage;
+using LojaSonhoDeCafe.Models.Dtos;
+using LojaSonhoDeCafe.Services.ProdutoServices;
+using LojaSonhoDeCafe.ServicesHttp.ProdutosHttpService;
+
+namespace LojaSonhoDeCafe.ServicesHttp.ProdutosLocalStorage
+{
+    public class LocalStorageProdutosService : ILocalStorageProdutosService
+    {
+        private const string Key = "ProdutoCollection";
+
+        private readonly IProdutoHttpService _produtoHttpService;
+        private readonly ILocalStorageService _localStorageService;
+        public LocalStorageProdutosService(IProdutoService produtoService,
+                                           ILocalStorageService localStorageService,
+                                           IProdutoHttpService produtoHttpService)
+        {
+            _localStorageService = localStorageService;
+            _produtoHttpService = produtoHttpService;
+        }
+
+        public async Task AtualizarDadosLocalStorage(IEnumerable<ProdutoDto> novosDados)
+        {
+            try
+            {
+                // Armazenar os novos dados no LocalStorage
+                await _localStorageService.SetItemAsync(Key, novosDados);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<IEnumerable<ProdutoDto>> GetCollection()
+        {
+            return await _localStorageService.GetItemAsync<IEnumerable<ProdutoDto>>(Key)
+                                                                ??
+                                                          await AddCollection();
+        }
+        public async Task RemoveCollection()
+        {
+            await _localStorageService.RemoveItemAsync(Key);
+        }
+        private async Task<IEnumerable<ProdutoDto>> AddCollection()
+        {
+            var listaProdutos = await _produtoHttpService.ObterProdutos();
+            if (listaProdutos != null)
+            {
+                await _localStorageService.SetItemAsync(Key, listaProdutos);
+            }
+
+            return listaProdutos!;
+        }
+
+    }
+}
