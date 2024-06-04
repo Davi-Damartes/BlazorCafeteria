30e64633e699e990e7fbd4ff05ab9a81e16ebcc6
Davi Lima <davidamartesgit@Gmail.com>
2024-06-04 19:28:44 -0300

    Referência ao novo nome Repositorio

---
+++ LojaSonhoDeCafe.Api/Repositories/Produtos/IProdutoRepositoryApi.cs
@@ -0,0 +1,29 @@
+﻿using LojaSonhoDeCafe.Models.Dtos;
+using LojaSonhoDeCafe.Models.Entity;
+
+namespace LojaSonhoDeCafe.Api.Repositories.Produtos
+{
+    public interface IProdutoRepositoryApi
+    {
+        Task<IEnumerable<Produto>> ObterTodosOsProdutos();
+
+        Task<Produto> ObterProdutoPorId(Guid Id);
+
+        Task<IEnumerable<Produto>> ObterTodosProdutosPorCategoria(int id);
+
+        Task<IEnumerable<Produto>> ObterProdutosFavoritos();
+
+        Task<IEnumerable<Categoria>> ObterCategorias();
+
+        Task AdicionarNovoProduto(Produto produto);
+
+        Task AdicionarEstoqueAoProduto(Guid Id, int Quantidade);
+
+        Task AtualizaProdutoFavorito(Produto produto);
+
+        Task ExcluirProduto(Guid Id);
+
+
+
+    }
+}
