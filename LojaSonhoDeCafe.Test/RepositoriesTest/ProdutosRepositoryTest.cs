5ec1dc4b6f5705159ae085140481d8df0271bd9f
Davi Lima <davidamartesgit@Gmail.com>
2024-06-04 19:30:45 -0300

    Nova classe para testar o Repositorio

---
+++ LojaSonhoDeCafe.Test/RepositoriesTest/ProdutosRepositoryTest.cs
@@ -0,0 +1,38 @@
+﻿
+
+using FakeItEasy;
+using FluentAssertions;
+using LojaSonhoDeCafe.Api.Repositories.Produtos;
+using LojaSonhoDeCafe.Models.Entity;
+
+namespace LojaSonhoDeCafe.Test.RepositoriesTest
+{
+    public class ProdutosRepositoryTest
+    {
+
+        private readonly IProdutoRepositoryApi _produtoRepository;
+        public ProdutosRepositoryTest()
+        {
+            _produtoRepository = A.Fake<IProdutoRepositoryApi>();
+        }
+
+        [Fact]
+        public async Task ProdutoRepository_AllProducts_ReturnIEnumerableProduto( )
+        {
+            //Arange
+            var produto = A.Fake<IEnumerable<Produto>>();
+
+            A.CallTo(( ) => _produtoRepository.ObterTodosOsProdutos())
+            .Returns(Task.FromResult(produto));
+
+
+            //Act
+            IEnumerable<Produto> result = await _produtoRepository.ObterTodosOsProdutos();
+
+            //Assert
+
+            result.Should().NotBeNull();
+        }
+
+    }
+}
