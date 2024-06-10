45f2cef68da201bbd6a104742311074f17c2faf3
Davi Lima <davidamartesgit@Gmail.com>
2024-06-10 20:44:50 -0300

    Handler para simular chamadas HttpService

---
+++ LojaSonhoDeCafe.Test/CarrinhoCompraTest/CarrinhoServiceHttpTest/HandlerHttp.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Json;
+using System.Net;
+
+namespace LojaSonhoDeCafe.Test.CarrinhoCompraTest.CarrinhoServiceHttpTest
+{
+    public class HandlerHttp : HttpMessageHandler
+    {
+        private readonly HttpStatusCode _statusCode;
+        private readonly object? _responseContent;
+
+        public HandlerHttp(HttpStatusCode statusCode, object? responseContent = null)
+        {
+            _statusCode = statusCode;
+            _responseContent = responseContent;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+                                                            HttpRequestMessage request, 
+                                                            CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(new HttpResponseMessage
+            {
+                StatusCode = _statusCode,
+                Content = JsonContent.Create(_responseContent)
+            });
+        }
+    }
+}
