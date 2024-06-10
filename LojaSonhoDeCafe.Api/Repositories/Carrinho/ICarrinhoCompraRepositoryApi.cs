170de43167845458c520f410cc67a16482583ba8
Davi Lima <davidamartesgit@Gmail.com>
2024-06-10 20:08:18 -0300

    Arrumando Layout

--- LojaSonhoDeCafe.Api/Repositories/Carrinho/ICarrinhoCompraRepositoryApi.cs
+++ LojaSonhoDeCafe.Api/Repositories/Carrinho/ICarrinhoCompraRepositoryApi.cs
@@ -4,19 +4,22 @@ using LojaSonhoDeCafe.Models.Entity;
 namespace LojaSonhoDeCafe.Api.Repositories.Carrinho
 {
     public interface ICarrinhoCompraRepositoryApi
+
     {
-        Task<CarrinhoItem> AdicionaItem(CarrinhoItemAdicionaDto carrinhoItemAdicionaDto);
+        Task<CarrinhoItem> ObtemItemDoCarrinhoPorId(int id);
+
+        Task<IEnumerable<CarrinhoItem>> ObtemItensDoCarinho(string usuarioId);
 
         Task<CarrinhoItem> AtualizaQuantidade(int id,
             CarrinhoItemAtualizaQuantidadeDto carrinhoItemAtualizaQuantidadeDto);
 
+
+        Task<CarrinhoItem> AdicionaItem(CarrinhoItemAdicionaDto carrinhoItemAdicionaDto);
+
+
         Task<CarrinhoItem> DeletaItem(int id);
 
         Task<bool> LimpaItensDoCarrinho();
-
-        Task<CarrinhoItem> ObtemItemDoCarrinhoPorId(int id);
-
-        Task<IEnumerable<CarrinhoItem>> ObtemItensDoCarinho(string usuarioId);
 
 
 
