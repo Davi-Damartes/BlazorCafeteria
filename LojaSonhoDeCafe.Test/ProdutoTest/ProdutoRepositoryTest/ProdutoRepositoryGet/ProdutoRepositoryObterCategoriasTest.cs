5a815e5e14fbcbc165ea0df8485c8c1bc62a0977
Davi Lima <davidamartesgit@Gmail.com>
2024-06-10 20:44:24 -0300

    Repositorio de teste para o Produto

---
+++ LojaSonhoDeCafe.Test/ProdutoTest/ProdutoRepositoryTest/ProdutoRepositoryGet/ProdutoRepositoryObterCategoriasTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using LojaSonhoDeCafe.Api.Repositories.Produtos;
+using LojaSonhoDeCafe.Models.Entity;
+using LojaSonhoDeCafe.Test.ProdutoTest.ProdutoRepositoriesTest.BancoInMemoryMetodo;
+
+namespace LojaSonhoDeCafe.Test.ProdutoTest.ProdutoRepositoriesTest.ProdutoRepositoryGet
+{
+    public class ProdutoRepositoryObterCategoriasTest
+    {
+
+        [Fact]
+        public async Task ProdutoRepository_ObterCategorias_ReturnaIEnumerableCategoria()
+        {
+            //Arange
+
+            var dbContext = await BancoDeDadosInMemory.GetBancoDeDado();
+
+            var repository = new ProdutoRepositoryApi(dbContext);
+
+            //Act
+            var result = repository.ObterCategorias();
+
+            //Assert         
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Task<IEnumerable<Categoria>>>();
+        }
+
+    }
+}
---
+++ LojaSonhoDeCafe.Test/ProdutoTest/ProdutoRepositoryTest/ProdutoRepositoryGet/ProdutoRepositoryObterTodosProdutosTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using LojaSonhoDeCafe.Api.Repositories.Produtos;
+using LojaSonhoDeCafe.Models.Entity;
+using LojaSonhoDeCafe.Test.ProdutoTest.ProdutoRepositoriesTest.BancoInMemoryMetodo;
+
+namespace LojaSonhoDeCafe.Test.ProdutoTest.ProdutoRepositoriesTest.ProdutoRepositoryGet
+{
+    public class ProdutoRepositoryObterTodosProdutosTest
+    {
+
+        [Fact]
+        public async Task ProdutoRepository_ObterTodosOsProdutos_ReturnaIEnumerableProduto()
+        {
+            //Arange
+            var dbContext = await BancoDeDadosInMemory.GetBancoDeDado();
+
+            var produtoRepository = new ProdutoRepositoryApi(dbContext);
+
+            //Act
+            var result = await produtoRepository.ObterTodosOsProdutos();
+            var teste = result.Count();
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<IEnumerable<Produto>>();
+            result.Should().HaveCountGreaterThan(0);
+            Assert.Equal(3, teste);
+        }
+
+    }
+}
