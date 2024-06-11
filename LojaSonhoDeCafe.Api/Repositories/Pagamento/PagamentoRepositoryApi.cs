d9b79678a3c2c2f427ff17dd3f4659dc2e9243df
Davi Lima <davidamartesgit@Gmail.com>
2024-06-11 19:29:32 -0300

    Organizando arquivo e removendo métodos

--- LojaSonhoDeCafe.Api/Repositories/Pagamento/PagamentoRepositoryApi.cs
+++ LojaSonhoDeCafe.Api/Repositories/Pagamento/PagamentoRepositoryApi.cs
@@ -3,6 +3,7 @@ using LojaSonhoDeCafe.Models.Entity;
 using LojaSonhoDeCafe.Api.Banco;
 using LojaSonhoDeCafe.Models.Dtos;
 using LojaSonhoDeCafe.Enum;
+using LojaSonhoDeCafe.MapeandoDtos.PagamentoConversao;
 
 namespace LojaSonhoDeCafe.Api.Repositories.Pagamento
 {
@@ -15,24 +16,45 @@ namespace LojaSonhoDeCafe.Api.Repositories.Pagamento
             _bancoDeDados = bancoDeDados;
         }
 
+
+
+        public async Task<IEnumerable<PagamentoDiario>> BucarTodosOsPagamentos()
+        {
+                    return await _bancoDeDados.PagamentosDiarios.AsQueryable()
+                                                                .Include(x => x.PagamentoProdutos)
+                                                                .OrderBy(x => x.HoraDoPagamento)
+                                                                .ToListAsync();
+            
+        }
+
+        public async Task<IEnumerable<PagamentoProduto>> BucarTodosOsPagamentosDeProdutos( )
+        {
+            return await _bancoDeDados.PagamentoProdutos.ToListAsync();
+        }
+
+
+
+        public async Task<IEnumerable<PagamentoDiario>> BucarTodosPagamentosPeloMes(int mes)
+        {
+
+            return await _bancoDeDados.PagamentosDiarios
+                                      .AsQueryable()
+                                        .Where(x => x.HoraDoPagamento.Month == mes)
+                                        .Include(x => x.PagamentoProdutos)
+                                        .OrderBy(x => x.HoraDoPagamento)
+                                        .ToListAsync();
+        }
+
+     
         public async Task<bool> AdicionarPagamento(PagamentoDiarioDto pagamentoDiarioDto)
         {
             if (pagamentoDiarioDto != null)
-            {
-                var pagamento = new PagamentoDiario
-                {
-                    Usuario = pagamentoDiarioDto.Usuario,
-                    Email = pagamentoDiarioDto.Email,
-                    TotalQuantDiaria = pagamentoDiarioDto.TotalQuantDiaria,
-                    TotalPrecoDiaria = pagamentoDiarioDto.TotalPrecoDiaria,
-                    EPagamento = (ETipoPagamento)pagamentoDiarioDto.EPagamento,
-                    HoraDoPagamento = pagamentoDiarioDto.HoraDoPagamento,
-
-                };
+            {             
+                var pagamento = pagamentoDiarioDto.ConvertePagamentoDtoParaPagamento();
 
                 pagamento = AdicionarProdutosNoPagamento(pagamento, pagamentoDiarioDto);
 
-
+                // Att Qnt do Produto no Banco de dados
                 await AtualizarQuantidadeProduto(pagamento);
 
                 await _bancoDeDados.PagamentosDiarios.AddAsync(pagamento);
@@ -42,54 +64,21 @@ namespace LojaSonhoDeCafe.Api.Repositories.Pagamento
             return false;
         }
 
-        public async Task<IEnumerable<PagamentoDiario>> BucarTodosOsPagamentosXXX()
+        public async Task ExcluirPagamento(Guid Id)
         {
-                    var result = await _bancoDeDados.PagamentosDiarios.AsQueryable()
-                                                                .Include(x => x.PagamentoProdutos)
-                                                                .OrderBy(x => x.HoraDoPagamento)
-                                                                .ToListAsync();
-            
-                    return result;
+            var pagamento = await _bancoDeDados.PagamentosDiarios
+                                               .AsQueryable()
+                                               .Include(x => x.PagamentoProdutos)
+                                               .FirstOrDefaultAsync(x => x.Id == Id);
+
+            if (pagamento != null)
+            {
+                _bancoDeDados.Remove(pagamento);
+                await _bancoDeDados.SaveChangesAsync();
+            }
         }
-
-        public async Task<IEnumerable<PagamentoProduto>> BucarTodosOsPagamentosProdutos( )
-        {
-            return await _bancoDeDados.PagamentoProdutos.ToListAsync();
-        }
-
-
-
-        public async Task<IEnumerable<PagamentoDiario>> BucarTodosPagamentosPeloMes(int mes)
-        {
-            
-            var resultado =  await _bancoDeDados.PagamentosDiarios
-                                      .AsQueryable()
-                                        .Where(x => x.HoraDoPagamento.Month == mes)
-                                        .Include(x => x.PagamentoProdutos)
-                                        .OrderBy(x => x.HoraDoPagamento)
-                                        .ToListAsync();
-            return resultado;
-        }
-
-
-        public async Task<IEnumerable<Produto>> ObterTodosProdutosPorCategoria(int id)
-        {
-            var produtos = await _bancoDeDados
-                              .Produtos
-                              .Include(a => a.Categoria)
-                              .Where(x => x.CategoriaId == id)
-                              .ToListAsync();
-
-            return produtos;
-        }
-
-        public async Task<IEnumerable<Categoria>> ObterCategorias( )
-        {
-            var categorias = await _bancoDeDados.Categorias.ToListAsync();
-
-            return categorias;
-        }
-
+      
+       
         public async Task<PagamentoDiario> BucarPagamentoPorId(Guid Id)
         {
             return await _bancoDeDados.PagamentosDiarios.FirstOrDefaultAsync(x => x.Id == Id) ?? null!;
@@ -107,11 +96,11 @@ namespace LojaSonhoDeCafe.Api.Repositories.Pagamento
                         ProdutoId = produtoDto.ProdutoId,
                         QuantidadeComprada = produtoDto.QuantidadeComprada,
                         ProdutoNome = produtoDto.ProdutoNome,
-                        PrecoTotal = produtoDto.PrecoTotal,                
+                        PrecoTotal = produtoDto.PrecoTotal,
                         PagamentoDiario = pagamento
                     };
 
-                    // Adicione o produto ao pagamento
+                    // Adicionando o produto ao Pagamento
                     pagamento.PagamentoProdutos!.Add(produto);
                 }
                 return pagamento;
@@ -136,18 +125,33 @@ namespace LojaSonhoDeCafe.Api.Repositories.Pagamento
             }
         }
 
-        public async Task ExcluirPagamento(Guid Id)
-        {
-            var pagamento = await _bancoDeDados.PagamentosDiarios
-                                         .AsQueryable()
-                                         .Include(x => x.PagamentoProdutos)
-                                         .FirstOrDefaultAsync(x => x.Id == Id);
-
-            if(pagamento != null)
-            {
-                _bancoDeDados.Remove(pagamento);
-                await _bancoDeDados.SaveChangesAsync();
-            }
-        }
+       
     }
 }
+
+//var pagamento = new PagamentoDiario
+//{
+//    Usuario = pagamentoDiarioDto.Usuario,
+//    Email = pagamentoDiarioDto.Email,
+//    TotalQuantDiaria = pagamentoDiarioDto.TotalQuantDiaria,
+//    TotalPrecoDiaria = pagamentoDiarioDto.TotalPrecoDiaria,
+//    EPagamento = (ETipoPagamento)pagamentoDiarioDto.EPagamento,
+//    HoraDoPagamento = pagamentoDiarioDto.HoraDoPagamento,
+
+//};
+
+
+//public async Task<IEnumerable<Categoria>> ObterCategorias( )
+//{
+//    return await _bancoDeDados.Categorias.ToListAsync();
+
+//}
+
+//public async Task<IEnumerable<Produto>> ObterTodosProdutosPorCategoria(int id)
+//{
+//    return await _bancoDeDados.Produtos
+//                              .Include(a => a.Categoria)
+//                              .Where(x => x.CategoriaId == id)
+//                              .ToListAsync();
+
+//}
