5156d06609f3b1d6cda7ca47bb180e6b2c7d22fc
Davi Lima <davidamartesgit@Gmail.com>
2024-06-11 19:35:42 -0300

    Métodos para testar a Exclusão de Dados no Repositorio.

---
+++ LojaSonhoDeCafe.Test/PagamentoTest/PagamentoRepositorioTest/PagamentoRepositorioDelete/PagamentoRepositorioDeleteTest.cs
@@ -0,0 +1,84 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using LojaSonhoDeCafe.Api.Repositories.Pagamento;
+using LojaSonhoDeCafe.Models.Entity;
+using LojaSonhoDeCafe.Test.PagamentoTest.PagamentoRepositorioTest.BancoInMemoryMetodo;
+
+namespace LojaSonhoDeCafe.Test.PagamentoTest.PagamentoRepositorioTest.PagamentoRepositorioDelete
+{
+    public class PagamentoRepositorioDeleteTest
+    {
+        [Fact]
+        public async Task PagRepositorio_ExcluirPagamento_IdValido_RetornaPagamentoVazio( )
+        {
+            // Arrange
+            var dbContext = await BancoDeDadosInMemoryPag .GetBancoDeDadoPag();
+
+            var listaPagamentoDiario = A.Fake<List<PagamentoProduto>>();
+
+            var pagamentoDiario = new PagamentoDiario
+            {
+                Usuario = "UsuarioTeste",
+                Email = "email@teste.com",
+                TotalQuantDiaria = 10,
+                TotalPrecoDiaria = 123.45m,
+                EPagamento = Enum.ETipoPagamento.paypal,
+                HoraDoPagamento = DateTime.Now,
+                PagamentoProdutos = listaPagamentoDiario
+            };
+
+            await dbContext.PagamentosDiarios.AddAsync(pagamentoDiario);
+            await dbContext.SaveChangesAsync();
+
+            var pagamentoRepositorio = new PagamentoRepositoryApi(dbContext);
+
+            // Act
+            await pagamentoRepositorio.ExcluirPagamento(pagamentoDiario.Id);
+
+            // Assert
+            pagamentoRepositorio.Should().NotBeNull();
+
+            var aposExclusao = await dbContext.PagamentosDiarios.FindAsync(pagamentoDiario.Id);
+            aposExclusao.Should().BeNull();
+            
+        }
+
+        [Fact]
+        public async Task PagRepositorio_ExcluirPagamento_IdInválido_ReturPagamento()
+        {
+            // Arrange
+            var dbContext = await BancoDeDadosInMemoryPag .GetBancoDeDadoPag();
+
+            var listaPagamentoDiario = A.Fake<List<PagamentoProduto>>();
+
+            var pagamentoDiario = new PagamentoDiario
+            {
+                Usuario = "UsuarioTeste",
+                Email = "email@teste.com",
+                TotalQuantDiaria = 10,
+                TotalPrecoDiaria = 123.45m,
+                EPagamento = Enum.ETipoPagamento.paypal,
+                HoraDoPagamento = DateTime.Now,
+                PagamentoProdutos = listaPagamentoDiario
+            };
+            var pagamentoId = pagamentoDiario.Id;
+
+
+            await dbContext.PagamentosDiarios.AddAsync(pagamentoDiario);
+            await dbContext.SaveChangesAsync();
+
+            var pagamentoRepositorio = new PagamentoRepositoryApi(dbContext);
+            pagamentoDiario.Id = Guid.Empty;
+            // Act
+            await pagamentoRepositorio.ExcluirPagamento(pagamentoDiario.Id);
+
+            // Assert
+            pagamentoRepositorio.Should().NotBeNull();
+
+            var aposExclusao = await dbContext.PagamentosDiarios.FindAsync(pagamentoId);
+            aposExclusao.Should().NotBeNull();
+            
+        }
+
+    }
+}
