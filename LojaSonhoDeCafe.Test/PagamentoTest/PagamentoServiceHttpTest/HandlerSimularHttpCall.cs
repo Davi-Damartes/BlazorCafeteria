acc38501a283f87387d82ad0267111e617f46e5c
Davi Lima <davidamartesgit@Gmail.com>
2024-06-12 15:32:48 -0300

    Handler para simular solicitações Http do PagamentoService com StatusCodes e Loggers.

---
+++ LojaSonhoDeCafe.Test/PagamentoTest/PagamentoServiceHttpTest/HandlerSimularHttpCall.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+using System.Net;
+
+namespace LojaSonhoDeCafe.Test.PagamentoTest.PagamentoServiceHttpTest
+{
+    public class HandlerSimularHttpCall : HttpMessageHandler
+    {
+        private readonly HttpStatusCode _statusCode;
+        private readonly object? _responseContent;
+
+        public HandlerSimularHttpCall(HttpStatusCode statusCode, object? responseContent = null)
+        {
+            _statusCode = statusCode;
+            _responseContent = responseContent;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(new HttpResponseMessage
+            {
+                StatusCode = _statusCode,
+                Content = JsonContent.Create(_responseContent)
+            });
+        }
+    }
+}
