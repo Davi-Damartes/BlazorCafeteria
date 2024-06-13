1346c19c42ed9d070578c674ed47a34e43b6896a
Davi Lima <davidamartesgit@Gmail.com>
2024-06-12 21:07:44 -0300

    Mapeanado Dtos

---
+++ LojaSonhoDeCafe.Models/MapeandoDtos/CarrinhoCompraMapping/CarrinhoCompraMap.cs
@@ -0,0 +1,68 @@
+﻿using LojaSonhoDeCafe.Models.Dtos;
+using LojaSonhoDeCafe.Models.Entity;
+
+namespace LojaSonhoDeCafe.Models.MapeandoDtos.CarrinhoCompraMapping
+{
+    public static class CarrinhoCompraMap
+    {
+        public static CarrinhoItemDto ConverterCarrinhoItemProdParaCarrinhoItemDto(this CarrinhoItem carrinhoItem,
+                                            Produto produto)
+        {
+            return new CarrinhoItemDto
+            {
+                Id = carrinhoItem.Id,
+                ProdutoId = carrinhoItem.ProdutoId,
+                ProdutoNome = produto.Nome,
+                ProdutoDescricao = produto.Descricao,
+                ProdutoFotoUrl = produto.FotoUrl,
+                Preco = produto.Preco,
+                CarrinhoId = carrinhoItem.CarrinhoId,
+                Quantidade = carrinhoItem.Quantidade, 
+                
+                PrecoTotal = produto.Preco * carrinhoItem.Quantidade
+            };
+        }
+
+        public static CarrinhoItemDto ConverteCarrinhoItemParaCarrinhoDto(this CarrinhoItem carrinhoItem,
+                                                                          Produto produto)
+        {
+            return new CarrinhoItemDto
+            {
+                Id = carrinhoItem.Id,
+                CarrinhoId = carrinhoItem.CarrinhoId,
+                ProdutoId = carrinhoItem.ProdutoId,
+                Quantidade = carrinhoItem.Quantidade,
+                ProdutoNome = produto?.Nome,
+                ProdutoDescricao = produto?.Descricao,
+                ProdutoFotoUrl = produto?.FotoUrl,
+                Preco = produto!.Preco,
+                PrecoTotal = (produto?.Preco ?? 0) * carrinhoItem.Quantidade
+            };
+        }
+
+
+        public static IEnumerable<CarrinhoItemDto> ConverterCarrinhoItensParaCarrinhoItensDto(
+        this IEnumerable<CarrinhoItem> carrinhoItens, IEnumerable<Produto> produtos)
+        {
+            return (from carrinhoItem in carrinhoItens
+                    join produto in produtos
+                    on carrinhoItem.ProdutoId equals produto.Id
+                    select new CarrinhoItemDto
+                    {
+                        Id = carrinhoItem.Id,
+                        ProdutoId = carrinhoItem.ProdutoId,
+                        ProdutoNome = produto.Nome,
+                        ProdutoDescricao = produto.Descricao,
+                        ProdutoFotoUrl = produto.FotoUrl,
+                        Preco = produto.Preco,
+                        CarrinhoId = carrinhoItem.CarrinhoId,
+                        Quantidade = carrinhoItem.Quantidade,
+                        QuantidadeEmEstoque = produto.QuantidadeEmEstoque,
+                        PrecoTotal = produto.Preco * carrinhoItem.Quantidade
+                    }).ToList();
+        }
+
+
+      
+    }
+}
---
+++ LojaSonhoDeCafe.Models/MapeandoDtos/PagamentoMapping/PagamentoMap.cs
@@ -0,0 +1,67 @@
+﻿using LojaSonhoDeCafe.Enum;
+using LojaSonhoDeCafe.Models.Dtos;
+using LojaSonhoDeCafe.Models.Entity;
+
+namespace LojaSonhoDeCafe.Models.MapeandoDtos.PagamentoConversao
+{
+    public static class PagamentoMap
+    {
+        public static PagamentoDiario ConvertePagamentoDtoParaPagamento(this PagamentoDiarioDto diarioDto)
+        {
+            return new PagamentoDiario
+            {
+                Usuario = diarioDto.Usuario,
+                Email = diarioDto.Email,
+                TotalQuantDiaria = diarioDto.TotalQuantDiaria,
+                TotalPrecoDiaria = diarioDto.TotalPrecoDiaria,
+                EPagamento = (ETipoPagamento)diarioDto.EPagamento,               
+                HoraDoPagamento = diarioDto.HoraDoPagamento,
+
+            };
+        }
+      
+        public static IEnumerable<PagamentoDiarioDto> ConverteListPagamentosParaListPagamentosDto(
+                                               this IEnumerable<PagamentoDiario> pagamentos)
+        {
+            return pagamentos.Select(pagamento => new PagamentoDiarioDto
+            {
+                Usuario = pagamento.Usuario,
+                Email = pagamento.Email,
+                TotalQuantDiaria = pagamento.TotalQuantDiaria,
+                TotalPrecoDiaria = pagamento.TotalPrecoDiaria,
+                EPagamento = (EPagamentoDto)pagamento.EPagamento,
+                HoraDoPagamento = pagamento.HoraDoPagamento,
+                ProdutosDoPagamento = pagamento.PagamentoProdutos!
+                              .Select(produto => new PagamentoProdutoDto
+                              {
+                                  ProdutoId = produto.ProdutoId,
+                                  QuantidadeComprada = produto.QuantidadeComprada,
+                                  ProdutoNome = produto.ProdutoNome,
+                                  PrecoTotal = produto.PrecoTotal
+                              }).ToList()
+            }).ToList();
+
+        }
+
+
+
+    }
+}
+
+
+//public static IEnumerable<PagamentoDiarioDto> ConvertListPagamentosParaListPagamentosDto(
+//                                        this IEnumerable<PagamentoDiario> pagamentos)
+//{
+//    return (from pagamento in pagamentos
+//            select new PagamentoDiarioDto
+//            {
+//                //ProdutoId = pagamento.ProdutoId,
+//                Usuario = pagamento.Usuario,
+//                Email = pagamento.Email,
+//                TotalQuantDiaria = pagamento.TotalQuantDiaria,
+//                TotalPrecoDiaria = pagamento.TotalPrecoDiaria,
+//                EPagamento = (EPagamentoDto)pagamento.EPagamento,
+//                HoraDoPagamento = pagamento.HoraDoPagamento,
+//            }).ToList();
+
+//}
