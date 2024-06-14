76f8e31bbef72f158c50320c448911860a996d16
Davi Lima <davidamartesgit@Gmail.com>
2024-06-14 19:24:15 -0300

    Comentando código para teste

--- LojaSonhoDeCafe/wwwroot/Js/BotaoClickFavorito.js
+++ LojaSonhoDeCafe/wwwroot/Js/BotaoClickFavorito.js
@@ -1,14 +1,36 @@
-﻿window.mudarCorButao = function (produtoId, isFavorito)
-{
-    var btn = document.getElementById('btn-' + produtoId);
-    var icone = btn.querySelector('.icone-coracao');
+﻿//window.mudarCorButao2 = function (produtoId, isFavorito)
+//{
+//    var btn = document.getElementById('btn-' + produtoId);
+//    var icone = btn.querySelector('.icone-coracao');
 
-    if (isFavorito == true) {
-        icone.style.color = 'red';
-    }
-    else {
-        icone.style.color = 'black';
-    }
-};
+//    if (isFavorito == true) {
+//        icone.style.color = 'red';
+//    }
+//    else {
+//        icone.style.color = 'black';
+//    }
+//};
+
+//export function mudarCorButao(produtoId, isFavorito) {
+//    var btn = document.getElementById('btn-' + produtoId);
+//    var icone = btn.querySelector('.icone-coracao');
+
+//    if (isFavorito == true) {
+//        icone.style.color = 'red';
+//    }
+//    else {
+//        icone.style.color = 'black';
+//    }
+//}
+
+
+
+//export function CorPadraoIconeFavorito(produtos,) {
+//    var btn = document.getElementById('btn-' + produtoId);
+//    var icone = btn.querySelector('.icone-coracao');
+
+//    icone.style.color = 'black';
+
+//}
 
 
