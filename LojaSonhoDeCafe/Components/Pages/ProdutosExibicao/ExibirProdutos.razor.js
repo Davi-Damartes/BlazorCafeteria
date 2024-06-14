78d06c1f03df20a9cfbbd3a184dfb8d4e1465f4f
Davi Lima <davidamartesgit@Gmail.com>
2024-06-14 19:29:56 -0300

    Add novo js quer será usado para controlar a cor do icone de Favoritar Produtos

---
+++ LojaSonhoDeCafe/Components/Pages/ProdutosExibicao/ExibirProdutos.razor.js
@@ -0,0 +1,11 @@
+﻿export function mudarCorButao(produtoId, isFavorito) {
+    var btn = document.getElementById('btn-' + produtoId);
+    var icone = btn.querySelector('.icone-coracao');
+
+    if (isFavorito == true) {
+        icone.style.color = 'red';
+    }
+    else {
+        icone.style.color = 'black';
+    }
+}
