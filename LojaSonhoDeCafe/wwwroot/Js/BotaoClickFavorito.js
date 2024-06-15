2ba207d60b97087eb6e50561d4ed43f22cc588d3
Davi Lima <davidamartesgit@Gmail.com>
2024-06-15 16:25:21 -0300

    Teste para js com Blazor

--- LojaSonhoDeCafe/wwwroot/Js/BotaoClickFavorito.js
+++ LojaSonhoDeCafe/wwwroot/Js/BotaoClickFavorito.js
@@ -11,26 +11,18 @@
 //    }
 //};
 
-//export function mudarCorButao(produtoId, isFavorito) {
-//    var btn = document.getElementById('btn-' + produtoId);
-//    var icone = btn.querySelector('.icone-coracao');
+export function mudarCorButao22(produtoId, isFavorito) {
+    var btn = document.getElementById('btn-' + produtoId);
+    var icone = btn.querySelector('.icone-coracao');
 
-//    if (isFavorito == true) {
-//        icone.style.color = 'red';
-//    }
-//    else {
-//        icone.style.color = 'black';
-//    }
-//}
+    if (isFavorito == true) {
+        icone.style.color = 'red';
+    }
+    else {
+        icone.style.color = 'black';
+    }
+}
 
 
-
-//export function CorPadraoIconeFavorito(produtos,) {
-//    var btn = document.getElementById('btn-' + produtoId);
-//    var icone = btn.querySelector('.icone-coracao');
-
-//    icone.style.color = 'black';
-
-//}
 
 
