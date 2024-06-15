f04c47358d7232047819984059f00a849ca4ec36
Davi Lima <davidamartesgit@Gmail.com>
2024-06-15 16:37:33 -0300

    Add código para ao clicar em uma forma de pagamento aplicar determinada formatação Css

--- LojaSonhoDeCafe/Components/PagamentoPage/PagamentoPersonalizado.razor.js
+++ LojaSonhoDeCafe/Components/PagamentoPage/PagamentoPersonalizado.razor.js
@@ -1,4 +1,16 @@
-﻿export function aplicarEstilos(divIdSelecionada, divIdOutra) {
+﻿export function inicializarPagamento() {
+    const divPagamentoCartao = document.getElementById('divPagamentoCartao');
+    const divPagamentoDinheiro = document.getElementById('divPagamentoDinheiro');
+
+    if (divPagamentoCartao && divPagamentoDinheiro) {
+        divPagamentoCartao.addEventListener('click', () => aplicarEstilos('divPagamentoCartao', 'divPagamentoDinheiro'));
+        divPagamentoDinheiro.addEventListener('click', () => aplicarEstilos('divPagamentoDinheiro', 'divPagamentoCartao'));
+    } else {
+        console.error('Elementos não encontrados: divPagamentoCartao ou divPagamentoDinheiro');
+    }
+}
+
+function aplicarEstilos(divIdSelecionada, divIdOutra) {
     const divSelecionada = document.getElementById(divIdSelecionada);
     const divOutra = document.getElementById(divIdOutra);
 
@@ -7,14 +19,15 @@
         const btnOutra = divOutra.querySelector('.btn-outline-danger');
 
         if (btnSelecionada) {
-            btnSelecionada.style.transition = 'background-color 0.2s ease';
+            btnSelecionada.style.transition = 'background-color 0.2s ease, color 0.2s ease';
             btnSelecionada.style.backgroundColor = 'var(--bs-danger)';
             btnSelecionada.style.color = 'white';
         }
 
         if (btnOutra) {
             btnOutra.style.transition = 'color 0.2s ease';
-            btnOutra.style.color = 'white';
+            btnOutra.style.color = 'black'; //
+            btnOutra.style.backgroundColor = '';
         }
     } else {
         console.error('Elementos não encontrados:', divIdSelecionada, divIdOutra);
