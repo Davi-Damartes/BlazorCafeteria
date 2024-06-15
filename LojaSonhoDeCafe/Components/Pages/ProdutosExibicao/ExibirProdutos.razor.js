580286ac41e22441a42bdd0e02baa101b8f92feb
Davi Lima <davidamartesgit@Gmail.com>
2024-06-15 16:27:26 -0300

    Add testes para js

--- LojaSonhoDeCafe/Components/Pages/ProdutosExibicao/ExibirProdutos.razor.js
+++ LojaSonhoDeCafe/Components/Pages/ProdutosExibicao/ExibirProdutos.razor.js
@@ -1,11 +1,24 @@
-﻿export function mudarCorButao(produtoId, isFavorito) {
-    var btn = document.getElementById('btn-' + produtoId);
-    var icone = btn.querySelector('.icone-coracao');
+﻿export function mudarCorButao(produtoid, isfavorito) {
+    var btn = document.getelementbyid('btn-' + produtoid);
+    var icone = btn.queryselector('.icone-coracao');
 
-    if (isFavorito == true) {
+    if (isfavorito == true) {
         icone.style.color = 'red';
     }
     else {
         icone.style.color = 'black';
     }
 }
+
+//window.mudarCorButao2 = function (produtoId, isFavorito)
+//{
+//    var btn = document.getElementById('btn-' + produtoId);
+//    var icone = btn.querySelector('.icone-coracao');
+
+//    if (isFavorito == true) {
+//        icone.style.color = 'red';
+//    }
+//    else {
+//        icone.style.color = 'black';
+//    }
+//};
