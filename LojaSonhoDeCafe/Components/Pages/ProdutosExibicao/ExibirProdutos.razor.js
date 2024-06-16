33a0613f90cf02f3459d17ad4966453416694832
Davi Lima <davidamartesgit@Gmail.com>
2024-06-16 16:54:55 -0300

    removendo código

--- LojaSonhoDeCafe/Components/Pages/ProdutosExibicao/ExibirProdutos.razor.js
+++ LojaSonhoDeCafe/Components/Pages/ProdutosExibicao/ExibirProdutos.razor.js
@@ -1,24 +1,12 @@
-﻿export function mudarCorButao(produtoid, isfavorito) {
-    var btn = document.getelementbyid('btn-' + produtoid);
-    var icone = btn.queryselector('.icone-coracao');
+﻿export function mudarCorButao22(produtoId, isFavorito) {
+    var btn = document.getElementById('btn-' + produtoId);
+    var icone = btn.querySelector('.icone-coracao');
 
-    if (isfavorito == true) {
+    if (isFavorito == true) {
         icone.style.color = 'red';
     }
     else {
         icone.style.color = 'black';
     }
-}
+};
 
-//window.mudarCorButao2 = function (produtoId, isFavorito)
-//{
-//    var btn = document.getElementById('btn-' + produtoId);
-//    var icone = btn.querySelector('.icone-coracao');
-
-//    if (isFavorito == true) {
-//        icone.style.color = 'red';
-//    }
-//    else {
-//        icone.style.color = 'black';
-//    }
-//};
